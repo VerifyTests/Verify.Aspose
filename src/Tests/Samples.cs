9b4c8fde09187c56863bfce6f01c4aaa8ce388ff
Simon Cropp <simon.cropp@gmail.com>
2019-12-05 09:10:15 +1100

    Update Samples.cs

--- src/Tests/Samples.cs
+++ src/Tests/Samples.cs
@@ -7,37 +7,47 @@ public class Samples :
     VerifyBase
 {
     #region VerifyPdf
+
     [Fact]
     public async Task VerifyPdf()
     {
         await this.VerifyPdf("sample.pdf");
     }
+
     #endregion
 
 #if DEBUG
+
     #region VerifyPowerPoint
+
     [Fact]
     public async Task VerifyPowerPoint()
     {
         await this.VerifyPowerPoint("sample.pptx");
     }
+
     #endregion
+
 #endif
 
     #region VerifyExcel
+
     [Fact]
     public async Task VerifyExcel()
     {
         await this.VerifyExcel("sample.xlsx");
     }
+
     #endregion
 
     #region VerifyWord
+
     [Fact]
     public async Task VerifyWord()
     {
         await this.VerifyWord("sample.docx");
     }
+
     #endregion
 
     public Samples(ITestOutputHelper output) :
