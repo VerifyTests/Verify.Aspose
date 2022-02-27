e77b1d6294ed2c8aee41e4e0124892732791aa48
Simon Cropp <simon.cropp@gmail.com>
2022-02-27 11:17:14 +1100

    refs

--- src/Verify.Aspose/VerifyAspose.cs
+++ src/Verify.Aspose/VerifyAspose.cs
@@ -1,26 +1,25 @@
 ﻿using Aspose.Cells;
 using Aspose.Slides;
 
-namespace VerifyTests
+namespace VerifyTests;
+
+public static partial class VerifyAspose
 {
-    public static partial class VerifyAspose
+    public static void Initialize()
     {
-        public static void Initialize()
-        {
-            VerifierSettings.RegisterFileConverter("xlsx", ConvertExcel);
-            VerifierSettings.RegisterFileConverter("xls", ConvertExcel);
-            VerifierSettings.RegisterFileConverter<Workbook>(ConvertExcel);
+        VerifierSettings.RegisterFileConverter("xlsx", ConvertExcel);
+        VerifierSettings.RegisterFileConverter("xls", ConvertExcel);
+        VerifierSettings.RegisterFileConverter<Workbook>(ConvertExcel);
 
-            VerifierSettings.RegisterFileConverter("pdf", ConvertPdf);
-            VerifierSettings.RegisterFileConverter<Aspose.Pdf.Document>(ConvertPdf);
+        VerifierSettings.RegisterFileConverter("pdf", ConvertPdf);
+        VerifierSettings.RegisterFileConverter<Aspose.Pdf.Document>(ConvertPdf);
 
-            VerifierSettings.RegisterFileConverter("pptx", ConvertPowerPoint);
-            VerifierSettings.RegisterFileConverter("ppt", ConvertPowerPoint);
-            VerifierSettings.RegisterFileConverter<Presentation>(ConvertPowerPoint);
+        VerifierSettings.RegisterFileConverter("pptx", ConvertPowerPoint);
+        VerifierSettings.RegisterFileConverter("ppt", ConvertPowerPoint);
+        VerifierSettings.RegisterFileConverter<Presentation>(ConvertPowerPoint);
 
-            VerifierSettings.RegisterFileConverter("docx", ConvertWord);
-            VerifierSettings.RegisterFileConverter("doc", ConvertWord);
-            VerifierSettings.RegisterFileConverter<Aspose.Words.Document>(ConvertWord);
-        }
+        VerifierSettings.RegisterFileConverter("docx", ConvertWord);
+        VerifierSettings.RegisterFileConverter("doc", ConvertWord);
+        VerifierSettings.RegisterFileConverter<Aspose.Words.Document>(ConvertWord);
     }
 }
